f0903ce343377bd3e394daaeaec107dd30e5769f
BrunosFeijo <bruno.s.feijo@hotmail.com>
2023-12-12 00:53:27 -0300

    Segunda Servlet

--- src/main/java/br/edu/ifrs/PrimeiraServlet.java
+++ src/main/java/br/edu/ifrs/PrimeiraServlet.java
@@ -1,12 +1,15 @@
 package br.edu.ifrs;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+//@WebServlet("/segundaServlet")
+@WebServlet(urlPatterns = {"/xpto", "/teste", "terceiraServlet"})
 public class PrimeiraServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
