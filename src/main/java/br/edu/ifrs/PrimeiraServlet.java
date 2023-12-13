b2ac14d589c682b86a22130d2b72933c3c6bc408
BrunosFeijo <bruno.s.feijo@hotmail.com>
2023-12-13 02:43:02 -0300

    correcao dependencia

--- src/main/java/br/edu/ifrs/PrimeiraServlet.java
+++ src/main/java/br/edu/ifrs/PrimeiraServlet.java
@@ -1,7 +1,5 @@
 package br.edu.ifrs;
 
-import br.com.ifrs.infra.ConnectionFactory;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
